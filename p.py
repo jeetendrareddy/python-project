70ded77f3cd017e16027641962095bb6c9c7cd63
jeetendra reddy <jeetendrareddy76@gmail.com>
2022-01-09 12:09:15 +0530

    changes has done

--- p.py
+++ p.py
@@ -1,4 +1,4 @@
 a = int(input("Enter the number first number to add:- "))
 b = int(input("Enter the number second number to add:- "))
-c = a+b
-print("The sum of two numbers are:- ",c)  
+sum = a+b
+print("The sum of two numbers are:- ",sum)   
